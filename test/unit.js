cce8528773de2ab9e21dace7df99c5c1e871d77f
Luke Childs <lukechilds123@gmail.com>
2019-01-11 13:28:22 +0700

    Test unsinged ints

--- test/unit.js
+++ test/unit.js
@@ -4,3 +4,14 @@ import byteRange from '..';
 test('byteRange is exported', t => {
 	t.not(byteRange, undefined);
 });
+
+test('byteRange(n, false) returns expected values for unsigned integers', t => {
+	const signed = false;
+
+	t.deepEqual(byteRange(1, signed), [0, 255]);
+	t.deepEqual(byteRange(2, signed), [0, 65535]);
+	t.deepEqual(byteRange(3, signed), [0, 16777215]);
+	t.deepEqual(byteRange(4, signed), [0, 4294967295]);
+	t.deepEqual(byteRange(5, signed), [0, 1099511627775]);
+	t.deepEqual(byteRange(6, signed), [0, 281474976710655]);
+});
