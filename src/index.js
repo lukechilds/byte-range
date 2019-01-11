a1553c3d03f44f93433ada272ff3a0925b695a71
Luke Childs <lukechilds123@gmail.com>
2019-01-11 12:58:11 +0700

    Add support for unsigned integers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-const byteRange = bytes => [0, Math.pow(2, (bytes * 8)) - 1];
+const byteRange = (bytes, signed = false) => {
+	let min = 0;
+	let max = Math.pow(2, (bytes * 8)) - 1;
+
+	if (signed) {
+		const offset = ((max + 1) / 2);
+		min -= offset;
+		max -= offset;
+	}
+
+	return [min, max];
+};
 
 module.exports = byteRange;
