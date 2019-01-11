12f3213628c79f61d2cc90903303abf6318f9f5f
Luke Childs <lukechilds123@gmail.com>
2019-01-11 13:31:28 +0700

    Test signed ints

--- test/unit.js
+++ test/unit.js
@@ -15,3 +15,14 @@ test('byteRange(n, false) returns expected values for unsigned integers', t => {
 	t.deepEqual(byteRange(5, signed), [0, 1099511627775]);
 	t.deepEqual(byteRange(6, signed), [0, 281474976710655]);
 });
+
+test('byteRange(n, true) returns expected values for signed integers', t => {
+	const signed = true;
+
+	t.deepEqual(byteRange(1, signed), [-128, 127]);
+	t.deepEqual(byteRange(2, signed), [-32768, 32767]);
+	t.deepEqual(byteRange(3, signed), [-8388608, 8388607]);
+	t.deepEqual(byteRange(4, signed), [-2147483648, 2147483647]);
+	t.deepEqual(byteRange(5, signed), [-549755813888, 549755813887]);
+	t.deepEqual(byteRange(6, signed), [-140737488355328, 140737488355327]);
+});
